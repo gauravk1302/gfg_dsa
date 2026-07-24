3668b9815a69c721a8646f837990e6580df56f8d
Gaurav Kumar <gauravsaini77069@gmail.com>
2026-07-24 23:57:12 +0530

    Sync GFG - Identical Linked Lists (Basic)

---
+++ GFG/Basic/0_IdenticalLinkedLists.java
@@ -0,0 +1,31 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/identical-linked-lists/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+/* Structure of a Node
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}*/
+class Solution {
+    public boolean areIdentical(Node head1, Node head2) {
+        // code here
+        while (head1 != null && head2 != null){
+            if (head1.data != head2.data){
+                return false;
+            }
+            
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        
+        return head1 == null && head2 == null;
+    }
+}
