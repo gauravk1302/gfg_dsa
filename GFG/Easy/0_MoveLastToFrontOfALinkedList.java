7de64cd00d13e0206d3fbcb928ee9a3478130418
Gaurav Kumar <gauravsaini77069@gmail.com>
2026-07-30 07:01:30 +0530

    Sync GFG - Move Last to Front of a Linked List (Easy)

--- GFG/Easy/0_MoveLastToFrontOfALinkedList.java
+++ GFG/Easy/0_MoveLastToFrontOfALinkedList.java
@@ -19,30 +19,24 @@ class Node
 class Solution {
     public static Node moveToFront(Node head) {
         // code here
-         if (head == null || head.next == null) {
+        if (head == null || head.next == null){
             return head;
         }
-      
-        // To keep track of the second last node
-        Node secLast = null; 
         
-        // To traverse to the last node
-        Node last = head;    
-
-        // Traverse the list to find the last and 
-        // second last nodes
-        while (last.next != null) {
+        Node secLast = null;
+        Node last = head;
+        
+        while (last.next != null){
             secLast = last;
             last = last.next;
         }
-
-        // Change the next of second last node to null
+        
         secLast.next = null;
-
-        // Make the last node as the new head
+        
+        
         last.next = head;
         head = last;
-
+        
         return head;
     }
 }
