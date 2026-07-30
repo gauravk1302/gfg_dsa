607d06d5cc13da2ce842ba8351cb192364d51181
Gaurav Kumar <gauravsaini77069@gmail.com>
2026-07-30 05:50:42 +0530

    Sync GFG - Identical Linked Lists (Basic)

--- GFG/Basic/0_IdenticalLinkedLists.java
+++ GFG/Basic/0_IdenticalLinkedLists.java
@@ -20,12 +20,13 @@ class Solution {
         while (head1 != null && head2 != null){
             if (head1.data != head2.data){
                 return false;
+                
             }
-            
-            head1 = head1.next;
-            head2 = head2.next;
+                head1 = head1.next;
+                head2 = head2.next;
+                
         }
         
-        return head1 == null && head2 == null;
+        return (head1 == null && head2 == null);
     }
 }
