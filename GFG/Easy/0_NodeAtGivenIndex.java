ae7bfc9cde666549d5c36d19623dc9e5d50f12cb
Gaurav Kumar <gauravsaini77069@gmail.com>
2026-08-02 22:03:36 +0530

    Sync GFG - Node at Given Index (Easy)

---
+++ GFG/Easy/0_NodeAtGivenIndex.java
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/node-at-a-given-index-in-linked-list/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public static int getNode(Node head, int k) {
+        Node curr = head;
+        int count = 1;
+
+        while (curr != null) {
+            if (count == k) {
+                return curr.data;
+            }
+            curr = curr.next;
+            count++;
+        }
+
+        return -1;
+    }
+}
